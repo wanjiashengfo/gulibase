c56d9eefa5ac72b6ae3b5578236e51a34e722fac
wanjiashengfo <493000667@qq.com>
2020-05-07 09:04:59 +0800

    45、商品服务-API-三级分类-查询-递归树形结构数据获取

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.atguigu.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +30,32 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithtree() {
+        List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
+        List<CategoryEntity> level1 = categoryEntities.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == 0;
+        }).map(menu -> {
+            menu.setChlidren(getChildren(menu,categoryEntities));
+            return menu;
+        }).sorted((menu1,menu2)->{
+            return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+        return level1 ;
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity root,List<CategoryEntity> all){
+        List<CategoryEntity> collect = all.stream().filter((category) -> {
+            return category.getParentCid() == root.getCatId();
+        }).map(categoryEntity->{
+            categoryEntity.setChlidren(getChildren(categoryEntity,all));
+            return categoryEntity;
+        }).sorted((menu1,menu2)->{
+            return (menu1.getSort()==null?0:menu1.getSort()) - (menu2.getSort()==null?0:menu2.getSort());
+        }).collect(Collectors.toList());
+        return collect;
     }
 
 }
