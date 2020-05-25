a233cb5eae78383cc3fb5b06ae615b3d51d58f46
wanjiashengfo <493000667@qq.com>
2020-05-25 22:00:06 +0800

    212、商城业务-认证服务-好玩的验证码倒计时

---
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.atguigu.gulimall.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+//    @GetMapping("/login.html")
+//    public String loginPage(){
+//        return "login";
+//    }
+//    @GetMapping("/reg.html")
+//    public String regPage(){
+//        return "reg";
+//    }
+}
