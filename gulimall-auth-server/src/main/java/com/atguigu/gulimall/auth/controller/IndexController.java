ae7924d98b9dcce17f83043ccc60353a179d8c3e
wanjiashengfo <493000667@qq.com>
2020-05-26 11:15:39 +0800

    215、商城业务-认证服务-一步一坑的注册页环境

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/IndexController.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/IndexController.java
@@ -1,16 +1,45 @@
 package com.atguigu.gulimall.auth.controller;
 
+import com.atguigu.common.constant.AuthServerConstant;
+import com.atguigu.common.exception.BizCodeEnume;
+import com.atguigu.common.utils.R;
+import com.atguigu.gulimall.auth.feign.ThirdPartFeignService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class IndexController {
-//    @GetMapping("/login.html")
-//    public String loginPage(){
-//        return "login";
-//    }
-//    @GetMapping("/reg.html")
-//    public String regPage(){
-//        return "reg";
-//    }
+    @Autowired
+    ThirdPartFeignService thirdPartFeignService;
+    @Autowired
+    StringRedisTemplate redisTemplate;
+    @ResponseBody
+    @GetMapping("/sms/sendCode")
+    public R sendCode(@RequestParam("phone") String phone){
+
+        String redisCode = redisTemplate.opsForValue().get(AuthServerConstant.SMS_CODE_CACHE_PREFIX + phone);
+        if(StringUtils.isNotEmpty(redisCode)){
+            long l = Long.parseLong(redisCode.split("_")[1]);
+            if(System.currentTimeMillis()-l<60000){
+                //60s内不能再发
+                return R.error(BizCodeEnume.VAILD_SMS_CODE_EXCEPTION.getCode(),BizCodeEnume.VAILD_SMS_CODE_EXCEPTION.getMsg());
+            }
+        }
+
+        //2.验证码的校验，redis.存key-phone,value-code
+       String code_org =  UUID.randomUUID().toString().substring(0,5);
+       String code = code_org +"_"+System.currentTimeMillis();
+       //redis缓存验证码，防止同一个phone 60s内再次发送验证码
+       redisTemplate.opsForValue().set(AuthServerConstant.SMS_CODE_CACHE_PREFIX+phone,code,10, TimeUnit.MINUTES);
+       thirdPartFeignService.sendCode(phone,code_org);
+       return R.ok();
+    }
 }
--- gulimall-common/src/main/java/com/atguigu/common/exception/BizCodeEnume.java
+++ gulimall-common/src/main/java/com/atguigu/common/exception/BizCodeEnume.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 public enum BizCodeEnume {
     VAILD_EXCEPTION(10001,"参数格式校验失败"),
     UNKNOW_EXCEPTION(10000,"系统未知异常"),
+    VAILD_SMS_CODE_EXCEPTION(10002,"验证码频率太高"),
     PRODUCT_UP_EXCEPTION(11000,"商品上架异常");
     @Getter
     private int code;
