369ecc87e08d3681c47346a11870cd62e7cdbc34
wanjiashengfo <493000667@qq.com>
2020-06-01 10:02:20 +0800

    255、商城业务-消息队列-SpringBoot整合RabbitMQ

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -1,8 +1,10 @@
 package com.atguigu.gulimall.order;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableRabbit
 @MapperScan("com.atguigu.gulimall.order.dao")
 @SpringBootApplication
 public class GulimallOrderApplication {
--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,11 +1,14 @@
 package com.atguigu.gulimall.order;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallOrderApplicationTests {
-
+	@Autowired
+	AmqpAdmin amqpAdmin;
 	@Test
 	void contextLoads() {
 	}
