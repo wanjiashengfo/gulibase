7cb0ff119d7862c4af569091591b9ba178d847f0
wanjiashengfo <493000667@qq.com>
2020-06-01 15:28:26 +0800

    257、商城业务-消息队列-RabbitTemplate使用

--- gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/atguigu/gulimall/order/GulimallOrderApplicationTests.java
@@ -1,18 +1,25 @@
 package com.atguigu.gulimall.order;
 
+import com.atguigu.gulimall.order.entity.OrderReturnReasonEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
 @Slf4j
 @SpringBootTest
 class GulimallOrderApplicationTests {
 	@Autowired
 	AmqpAdmin amqpAdmin;
+	@Autowired
+	RabbitTemplate rabbitTemplate;
 	@Test
 	void createExchange() {
 		DirectExchange directExchange = new DirectExchange("hello-java-exchange",true,false);
@@ -34,5 +41,14 @@ class GulimallOrderApplicationTests {
 				"hello.java",null);
 		amqpAdmin.declareBinding(binding);
 		log.info("binding【{}】创建成功","hello-java-queue");
+	}
+	@Test
+	public void sendMessageTest(){
+		OrderReturnReasonEntity reasonEntity = new OrderReturnReasonEntity();
+		reasonEntity.setId(1L);
+		reasonEntity.setCreateTime(new Date());
+		reasonEntity.setName("哈哈");
+		rabbitTemplate.convertAndSend("hello-java-exchange","hello.java",reasonEntity);
+		log.info("信息【{}】发送成功",reasonEntity);
 	}
 }
