bcfbb90fb76125b0c72660922833e54c4780c040
wanjiashengfo <493000667@qq.com>
2020-06-02 17:01:19 +0800

    264、商城业务-订单服务-订单登录拦截

---
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/web/OrderWebController.java
@@ -0,0 +1,12 @@
+package com.atguigu.gulimall.order.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OrderWebController {
+    @GetMapping("/toTrade")
+    public String toTrade(){
+        return "confirm";
+    }
+}
