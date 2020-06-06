104da1d434eb2d073fe0149de1d4e0e53b260cad
wanjiashengfo <493000667@qq.com>
2020-06-07 02:58:10 +0800

    292、商城业务-订单服务-延时队列定时关单模拟

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/web/HelloController.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/web/HelloController.java
@@ -1,13 +1,31 @@
 package com.atguigu.gulimall.order.web;
 
+import com.atguigu.gulimall.order.entity.OrderEntity;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+import java.util.UUID;
 
 @Controller
 public class HelloController {
+    @Autowired
+    RabbitTemplate rabbitTemplate;
     @GetMapping("/{page}.html")
     public String listPage(@PathVariable("page") String page){
         return page;
+    }
+    @ResponseBody
+    @GetMapping("/test/createOrder")
+    public String createOrderTest(){
+        OrderEntity entity = new OrderEntity();
+        entity.setOrderSn(UUID.randomUUID().toString());
+        entity.setModifyTime(new Date());
+        rabbitTemplate.convertAndSend("order-event-exchange","order.create.order",entity);
+        return  "ok";
     }
 }
