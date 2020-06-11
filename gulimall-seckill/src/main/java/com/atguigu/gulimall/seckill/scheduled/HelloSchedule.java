0c976ad19b3a2047ac6a07e6e940b3092d42b85e
wanjiashengfo <493000667@qq.com>
2020-06-11 10:35:17 +0800

    @Slf4j @Component @EnableScheduling @EnableAsync public class HelloSchedule {     @Async     @Scheduled(cron = "*/5 * * ? * 4")     public void hello(){         log.info("hello....");     } 312、商城业务-秒杀服务-SpringBoot整合定时任务与异步任务

--- gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/scheduled/HelloSchedule.java
+++ gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/scheduled/HelloSchedule.java
@@ -1,4 +1,19 @@
 package com.atguigu.gulimall.seckill.scheduled;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+@Slf4j
+@Component
+@EnableScheduling
+@EnableAsync
 public class HelloSchedule {
+    @Async
+    @Scheduled(cron = "*/5 * * ? * 4")
+    public void hello(){
+        log.info("hello....");
+    }
 }
