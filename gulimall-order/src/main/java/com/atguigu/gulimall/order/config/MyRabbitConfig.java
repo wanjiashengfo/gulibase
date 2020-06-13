59eb418b9324f28966d0ff51cc3fa5b994a2f1b7
wanjiashengfo <493000667@qq.com>
2020-06-13 15:07:08 +0800

    329、Sentinel-全服务引入

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/config/MyRabbitConfig.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/config/MyRabbitConfig.java
@@ -1,6 +1,7 @@
 package com.atguigu.gulimall.order.config;
 
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -8,18 +9,32 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.annotation.PostConstruct;
 
 @Configuration
 public class MyRabbitConfig {
-    @Autowired
+
     RabbitTemplate rabbitTemplate;
+//    public MyRabbitConfig(RabbitTemplate rabbitTemplate){
+//        this.rabbitTemplate = rabbitTemplate;
+//        initRabbitTemplate();
+//    }
+    @Primary
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        this.rabbitTemplate = rabbitTemplate;
+        rabbitTemplate.setMessageConverter(messageConverter());
+        initRabbitTemplate();
+        return rabbitTemplate;
+    }
     @Bean
     public MessageConverter messageConverter(){
         return new Jackson2JsonMessageConverter();
     }
-    @PostConstruct //MyRabbitConfig对象创建完成以后，执行这个方法
+//    @PostConstruct //MyRabbitConfig对象创建完成以后，执行这个方法
     public void initRabbitTemplate(){
         rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
             @Override
