1e2c4ba6f0fb2a270d38404c581c990713ea9cad
wanjiashengfo <493000667@qq.com>
2020-06-13 10:23:16 +0800

    324、商城业务-秒杀服务-秒杀页面完成

--- gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/controller/SeckillController.java
+++ gulimall-seckill/src/main/java/com/atguigu/gulimall/seckill/controller/SeckillController.java
@@ -5,22 +5,22 @@ import com.atguigu.gulimall.seckill.service.SeckillService;
 import com.atguigu.gulimall.seckill.to.SecKillSkuRedisTo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class SeckillController {
     @Autowired
     SeckillService seckillService;
+    @ResponseBody
     @GetMapping("/currentSeckillSkus")
     public R getCurrentSeckillSkus(){
         List<SecKillSkuRedisTo> vos = seckillService.getCurrentSeckillSkus();
         return R.ok().setData(vos);
     }
+    @ResponseBody
     @GetMapping("/sku/seckill/{skuId}")
     public R getSkuSeckillInfo(@PathVariable("skuId") Long skuId){
         SecKillSkuRedisTo to = seckillService.getSkuSeckillInfo(skuId);
@@ -28,10 +28,12 @@ public class SeckillController {
     }
 
     @GetMapping("/kill")
-    public  R secKill(@RequestParam("killId") String killId,
+    public  String secKill(@RequestParam("killId") String killId,
                       @RequestParam("key") String key,
-                      @RequestParam("num") Integer num){
+                      @RequestParam("num") Integer num,
+                           Model model){
         String orderSn = seckillService.kill(killId,key,num);
-        return R.ok().setData(orderSn);
+        model.addAttribute("orderSn",orderSn);
+        return "success";
     }
 }
